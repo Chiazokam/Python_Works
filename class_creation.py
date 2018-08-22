8052f89a40c28fe0c132a7cb983300e47def4b06
chiazokam <chiazokamecheta@gmail.com>
2018-08-22 10:01:45 +0100

    Initial Commit: Class creation file

--- class_creation.py
+++ class_creation.py
@@ -7,31 +7,42 @@ Created on Thu Aug 16 17:12:52 2018
 
 class Car:
     
+    #Class Variable
+    car_manufacturer = "Stevie Combs"
+    
+    #Also defines the instance variables
     def __init__ (self, color, width, fly):
         
-        self.color = "yellow"
+        self.color = color
         
-        self.width = 20
+        self.width = width
         
-        self.fly = True
+        self.fly = fly
         
+        print("This is the car class with color", self.color)
+      
+        #Also defines the instance variable, color
     def change(self, color):
         
         self.color = color
-        print("The color chosen here is", color)
-        
+        print("The color chosen here is", self.color)
+      
+        #Also defines the instance variable, fly
     def jump(self, fly):
         
         self.fly = fly
-        print("The fly feature of this car is", fly)
+        print("The fly feature of this car is", self.fly)
         
 #benz = Car()
 
-g = Car
+Innoson = Car("purple", 50, "False")
 
-Innoson = g("green", 50, "False")
+Benz = Car("burgundy", 25, "True")
 
-print(Innoson.jump("True"))
+#Innoson.jump("True")
 
+print(Innoson.color, Innoson.car_manufacturer)
+
+print(Benz.color, Benz.car_manufacturer)
 
 
